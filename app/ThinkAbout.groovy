069a56752dab31ba5e50548c1d9bf6f339a95126
Jonatas Emidio <jonatasemidio@gmail.com>
2015-06-30 15:03:54 -0300

    Update ThinkAbout.groovy
    Put the mechanism in a class

--- app/ThinkAbout.groovy
+++ app/ThinkAbout.groovy
@@ -1,40 +1,42 @@
-def this() {
-
-advice = [ 
-      'Data will help us remember, but will it let us forget?'                                                                                        ,
-      'It will help politicians get elected, but will it help them lead?'                                                                                 ,
-      'It will help companies make products addictive, but will it help us get free once we\'re hooked?'                                                  ,
-      'It will help advertisers see people as statistics, but will it help us remember those statistics are people?'                                      ,
-      'It will help bank prevent credit card fraud, but will it help us stay out of debt?'                                                                ,
-      'It will help credit card companies predict the impending collapse of a marriage, but will it keep our marriages form falling apart?'               ,
-      'It will help parents make kids genetically perfect, but will it help us love them regardless?'                                                     ,
-      'It will help higt-frequency traders sell stocks in nanoseconds, but will it help protect markets from feedback loopsin their programs?'            ,
-      'It will help meteorologist predict storm and tornadoes, but will it help us rebuild the homes of survivors?'                                       ,
-      'It will help biologists map the migration of fish, but will it keep us from overfishing the oceans?'                                               ,
-      'It will help phsicists find the "God particle" in a supercollider, but will it help us agree about God?'                                           ,
-      'It will help astronomers search for sign of alien life, but will it help us know if aliens are friendly or mean?'                                  ,
-      'It will help cardiologist monitor pacemakers with wiki connections, but will it keep hackers from hacking our hearts?'                             ,
-      'It will help virologist publish the genome of major disease, but will  it keep terrorists from developing weaponizade straisn?'                    ,
-      'It will help soldiers kill enemies remotely with drones, but will it help us see war as more than a game?'                                         ,
-      'It will help urbanists develop "smart cities," but what will become of our towns ?'                                                                ,
-      'It will help governments map the consumption patterns of cities, but will it help us depend less on consuming?'                                    ,
-      'It will help hackers leak evidence of government surveillance, but will we treat those hackers as heroes or thieves?'                              ,
-      'It will help police triangulate the location of gunshots, but will it help us address the underlying causes of violence?'                          ,
-      'It will help educators make excellent standardized tests, but will it help us embrace different standards of excellence ?'                         ,
-      'It will help farmers engineer crops to produce bigger yields, but will it keep corperations from patenting our food?'                              ,
-      'It will help search engines know how often people searchfor "love," but will it help people find it?'                                              ,
-      'It will help singles plan a hundred first dates , but will it help then know when they\'ve found the right person?'                               ,
-      'It will help pet owners clone their dogs and their cats, but will it help us love the clones as much as the cloned?'                               ,
-      'It will help neurologist implant chips in our brains, but will it help us turn off the chatter?'                                                   ,
-      'It will help geneticists sequence our genome, but will it help us understand who we are?'                                                          ,
-      'It will help us fell connected, but will it help us feel loved?'                                                                                   ,
-      'It will help us uncover the facts, but will it help us be wise?'                                                                                   ,
-      'It will help us live forever, but will it help us see that life\'s meaning stems from the fact that it ends?'                                       ,
-      'It will help us keep count of everything in our lives, but will it help us understand that not everything that counts in our lives can be counted?',
-      'It will help us see the world as it is, but will it help us see the world as it could be?'
-    ]
-
-  Collections.shuffle(advice)
-  
-  advice[0]
+public class ThinkAbout {
+      public this() {
+      
+            advice = [ 
+                  'Data will help us remember, but will it let us forget?'                                                                                        ,
+                  'It will help politicians get elected, but will it help them lead?'                                                                                 ,
+                  'It will help companies make products addictive, but will it help us get free once we\'re hooked?'                                                  ,
+                  'It will help advertisers see people as statistics, but will it help us remember those statistics are people?'                                      ,
+                  'It will help bank prevent credit card fraud, but will it help us stay out of debt?'                                                                ,
+                  'It will help credit card companies predict the impending collapse of a marriage, but will it keep our marriages form falling apart?'               ,
+                  'It will help parents make kids genetically perfect, but will it help us love them regardless?'                                                     ,
+                  'It will help higt-frequency traders sell stocks in nanoseconds, but will it help protect markets from feedback loopsin their programs?'            ,
+                  'It will help meteorologist predict storm and tornadoes, but will it help us rebuild the homes of survivors?'                                       ,
+                  'It will help biologists map the migration of fish, but will it keep us from overfishing the oceans?'                                               ,
+                  'It will help phsicists find the "God particle" in a supercollider, but will it help us agree about God?'                                           ,
+                  'It will help astronomers search for sign of alien life, but will it help us know if aliens are friendly or mean?'                                  ,
+                  'It will help cardiologist monitor pacemakers with wiki connections, but will it keep hackers from hacking our hearts?'                             ,
+                  'It will help virologist publish the genome of major disease, but will  it keep terrorists from developing weaponizade straisn?'                    ,
+                  'It will help soldiers kill enemies remotely with drones, but will it help us see war as more than a game?'                                         ,
+                  'It will help urbanists develop "smart cities," but what will become of our towns ?'                                                                ,
+                  'It will help governments map the consumption patterns of cities, but will it help us depend less on consuming?'                                    ,
+                  'It will help hackers leak evidence of government surveillance, but will we treat those hackers as heroes or thieves?'                              ,
+                  'It will help police triangulate the location of gunshots, but will it help us address the underlying causes of violence?'                          ,
+                  'It will help educators make excellent standardized tests, but will it help us embrace different standards of excellence ?'                         ,
+                  'It will help farmers engineer crops to produce bigger yields, but will it keep corperations from patenting our food?'                              ,
+                  'It will help search engines know how often people searchfor "love," but will it help people find it?'                                              ,
+                  'It will help singles plan a hundred first dates , but will it help then know when they\'ve found the right person?'                               ,
+                  'It will help pet owners clone their dogs and their cats, but will it help us love the clones as much as the cloned?'                               ,
+                  'It will help neurologist implant chips in our brains, but will it help us turn off the chatter?'                                                   ,
+                  'It will help geneticists sequence our genome, but will it help us understand who we are?'                                                          ,
+                  'It will help us fell connected, but will it help us feel loved?'                                                                                   ,
+                  'It will help us uncover the facts, but will it help us be wise?'                                                                                   ,
+                  'It will help us live forever, but will it help us see that life\'s meaning stems from the fact that it ends?'                                       ,
+                  'It will help us keep count of everything in our lives, but will it help us understand that not everything that counts in our lives can be counted?',
+                  'It will help us see the world as it is, but will it help us see the world as it could be?'
+                ]
+            
+              Collections.shuffle(advice)
+              
+              advice[0]
+      }
 }
